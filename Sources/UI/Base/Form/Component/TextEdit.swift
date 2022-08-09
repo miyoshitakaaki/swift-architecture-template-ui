89d0f7ea390abd5063d440550ef04f3bdb595f62
miyoshi-cq <t.miyoshi@caraquri.com>
2022-08-09 11:47:49 +0900

    modify input form style

--- Sources/UI/Base/Form/Component/TextEdit.swift
+++ Sources/UI/Base/Form/Component/TextEdit.swift
@@ -8,38 +8,42 @@ public final class TextEdit<T: UIControl>: Publisher where T: Publisher, T.Outpu
     public typealias Failure = Never
 
     private let titleStyle: ViewStyle<UILabel>
+    private let inValidTitleStyle: ViewStyle<UILabel>
     private let initialTitle: String
-    private let allowEmpty: Bool
     private let isPhone: Bool
     private let isEmail: Bool
     private let isPassword: Bool
+    private let isRequired: Bool
+    private let requireColor: UIColor
 
     public let titleLabel: UILabel
     public let edit: T
 
     public init(
         titleStyle: ViewStyle<UILabel>,
+        inValidTitleStyle: ViewStyle<UILabel>,
         title: String,
         edit: T,
-        allowEmpty: Bool = true,
         isPhone: Bool = false,
         isEmail: Bool = false,
         isPassword: Bool = false,
         isRequired: Bool = false,
         requireColor: UIColor = .red
     ) {
-        self.initialTitle = title
-        self.allowEmpty = allowEmpty
+        self.titleStyle = titleStyle
+        self.inValidTitleStyle = inValidTitleStyle
+        self.initialTitle = isRequired ? title + "＊" : title
         self.isPhone = isPhone
+        self.isRequired = isRequired
         self.isEmail = isEmail
         self.isPassword = isPassword
         self.titleLabel = .init(
             style: titleStyle,
-            title: isRequired ? title + "＊" : title
+            title: self.initialTitle
         )
         self.titleLabel.setSubTextColor(text: "＊", color: requireColor)
         self.edit = edit
-        self.titleStyle = titleStyle
+        self.requireColor = requireColor
     }
 
     public var isEnabled = true {
@@ -58,44 +62,33 @@ public final class TextEdit<T: UIControl>: Publisher where T: Publisher, T.Outpu
 
                 guard let self = self else { return }
 
-                if self.allowEmpty == false, text.isEmpty, self.isEnabled {
+                if self.isRequired, text.isEmpty, self.isEnabled {
                     self.titleLabel.text = self.initialTitle + "(必ず入力してください)"
-                    self.titleLabel.apply(.init {
-                        $0.textColor = UIConfig.dangerRed
-                        $0.font = UIFont.boldSystemFont(ofSize: 12)
-                    })
+                    self.titleLabel.apply(self.inValidTitleStyle)
                     return
                 }
 
                 if self.isPhone, text.isValid(regex: .phone) == false, self.isEnabled {
                     self.titleLabel.text = self.initialTitle + "(正しい書式で入力してください)"
-                    self.titleLabel.apply(.init {
-                        $0.textColor = UIConfig.dangerRed
-                        $0.font = UIFont.boldSystemFont(ofSize: 12)
-                    })
+                    self.titleLabel.apply(self.inValidTitleStyle)
                     return
                 }
 
                 if self.isEmail, text.isValid(regex: .email) == false {
                     self.titleLabel.text = self.initialTitle + "(正しい書式で入力してください)"
-                    self.titleLabel.apply(.init {
-                        $0.textColor = UIConfig.dangerRed
-                        $0.font = UIFont.boldSystemFont(ofSize: 12)
-                    })
+                    self.titleLabel.apply(self.inValidTitleStyle)
                     return
                 }
 
                 if self.isPassword, text.isValid(regex: .password) == false {
                     self.titleLabel.text = self.initialTitle + "(半角英数字8文字以上で入力してください)"
-                    self.titleLabel.apply(.init {
-                        $0.textColor = UIConfig.dangerRed
-                        $0.font = UIFont.boldSystemFont(ofSize: 12)
-                    })
+                    self.titleLabel.apply(self.inValidTitleStyle)
                     return
                 }
 
                 self.titleLabel.text = self.initialTitle
                 self.titleLabel.apply(self.titleStyle)
+                self.titleLabel.setSubTextColor(text: "＊", color: self.requireColor)
             })
             .subscribe(subscriber)
     }

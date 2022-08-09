48e95b378c93c965cf5ae24a49c59ab963dea0e1
miyoshi-cq <t.miyoshi@caraquri.com>
2022-08-09 12:04:23 +0900

    refactoring

--- Sources/UI/Base/Form/Component/TextEdit.swift
+++ Sources/UI/Base/Form/Component/TextEdit.swift
@@ -4,15 +4,17 @@ import UIKit
 public final class TextEdit<T: UIControl>: Publisher where T: Publisher, T.Output == String,
     T.Failure == Never
 {
+    public enum InputType {
+        case normal, phone, email, password, postalCode
+    }
+
     public typealias Output = String
     public typealias Failure = Never
 
     private let titleStyle: ViewStyle<UILabel>
     private let inValidTitleStyle: ViewStyle<UILabel>
     private let initialTitle: String
-    private let isPhone: Bool
-    private let isEmail: Bool
-    private let isPassword: Bool
+    private let inputType: InputType
     private let isRequired: Bool
     private let requireColor: UIColor
 
@@ -24,26 +26,23 @@ public final class TextEdit<T: UIControl>: Publisher where T: Publisher, T.Outpu
         inValidTitleStyle: ViewStyle<UILabel>,
         title: String,
         edit: T,
-        isPhone: Bool = false,
-        isEmail: Bool = false,
-        isPassword: Bool = false,
+        inputType: InputType = .normal,
         isRequired: Bool = false,
         requireColor: UIColor = .red
     ) {
         self.titleStyle = titleStyle
         self.inValidTitleStyle = inValidTitleStyle
         self.initialTitle = isRequired ? title + "＊" : title
-        self.isPhone = isPhone
+        self.inputType = inputType
         self.isRequired = isRequired
-        self.isEmail = isEmail
-        self.isPassword = isPassword
+        self.requireColor = requireColor
+
         self.titleLabel = .init(
             style: titleStyle,
             title: self.initialTitle
         )
         self.titleLabel.setSubTextColor(text: "＊", color: requireColor)
         self.edit = edit
-        self.requireColor = requireColor
     }
 
     public var isEnabled = true {
@@ -68,22 +67,30 @@ public final class TextEdit<T: UIControl>: Publisher where T: Publisher, T.Outpu
                     return
                 }
 
-                if self.isPhone, text.isValid(regex: .phone) == false, self.isEnabled {
+                switch self.inputType {
+                case .normal:
+                    break
+
+                case .phone where text.isValid(regex: .phone) == false && self.isEnabled:
                     self.titleLabel.text = self.initialTitle + "(正しい書式で入力してください)"
                     self.titleLabel.apply(self.inValidTitleStyle)
                     return
-                }
 
-                if self.isEmail, text.isValid(regex: .email) == false {
+                case .email where text.isValid(regex: .email) == false:
                     self.titleLabel.text = self.initialTitle + "(正しい書式で入力してください)"
                     self.titleLabel.apply(self.inValidTitleStyle)
                     return
-                }
 
-                if self.isPassword, text.isValid(regex: .password) == false {
+                case .password where text.isValid(regex: .password) == false:
                     self.titleLabel.text = self.initialTitle + "(半角英数字8文字以上で入力してください)"
                     self.titleLabel.apply(self.inValidTitleStyle)
                     return
+
+                case .postalCode:
+                    break
+
+                default:
+                    break
                 }
 
                 self.titleLabel.text = self.initialTitle

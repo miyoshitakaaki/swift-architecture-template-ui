c9bd0cec44ad9597748b4205d1b9b80e74284e28
miyoshi-cq <t.miyoshi@caraquri.com>
2022-08-09 11:16:15 +0900

    add required input setting

---
+++ Sources/UI/Utility/UILabel+Extension.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+extension UILabel {
+    func setSubTextColor(text: String, color: UIColor) {
+        let attributedString = NSMutableAttributedString(string: self.text!)
+        let range = attributedString.mutableString.range(
+            of: text,
+            options: NSString.CompareOptions.caseInsensitive
+        )
+        if range.location != NSNotFound {
+            attributedString.addAttribute(
+                NSAttributedString.Key.foregroundColor,
+                value: color,
+                range: range
+            )
+        }
+        self.attributedText = attributedString
+    }
+}

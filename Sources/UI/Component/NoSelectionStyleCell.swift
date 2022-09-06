510486f4a5ce1991329c763a5d792b697d6f0e8d
miyoshi-cq <t.miyoshi@caraquri.com>
2022-09-06 22:31:47 +0900

    add NoSelectionStyleCell

---
+++ Sources/UI/Component/NoSelectionStyleCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+@available(iOS 14.0, *)
+open class NoSelectionStyleCell: UICollectionViewListCell {
+    override public func updateConfiguration(using state: UICellConfigurationState) {
+        if state.isSelected || state.isHighlighted {
+            var back = UIBackgroundConfiguration.listPlainCell().updated(for: state)
+            let v = UIView()
+            if state.isSelected || state.isHighlighted {
+                let v2 = UIView()
+                v2.backgroundColor = .white
+                v.addSubview(v2)
+                v2.frame = v.bounds
+                v2.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            }
+            back.customView = v
+
+            self.backgroundConfiguration = back
+        }
+        super.updateConfiguration(using: state)
+    }
+}

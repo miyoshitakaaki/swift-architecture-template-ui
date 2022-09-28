7e7d639c5df45d57822842d31f76c15a03b162d2
miyoshi-cq <t.miyoshi@caraquri.com>
2022-09-28 14:30:32 +0900

    modify AlertPresentable

--- Sources/UI/Utility/AlertPresentable.swift
+++ Sources/UI/Utility/AlertPresentable.swift
@@ -3,10 +3,21 @@ import Utility
 
 public protocol AlertPresentable {
     func present(_ error: AppError)
+    func present(title: String, message: String, action: @escaping (UIAlertAction) -> Void)
     func didAuthOKButtonTapped()
 }
 
 public extension AlertPresentable where Self: UIViewController {
+    func present(title: String, message: String, action: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        alert.addAction(.init(title: "OK", style: .cancel, handler: action))
+        self.present(alert, animated: true)
+    }
+    
     func present(_ error: AppError) {
         switch error {
         case let .normal(title, message):

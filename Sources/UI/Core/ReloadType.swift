a3758508e88e92974569b995438d117c3cd12105
miyoshi-cq <t.miyoshi@caraquri.com>
2023-01-06 16:41:13 +0900

    modify reload

--- Sources/UI/Core/ReloadType.swift
+++ Sources/UI/Core/ReloadType.swift
@@ -1,5 +1,5 @@
 import UIKit
 
 public enum ReloadType {
-    case local, remoteOnlySection(sections: [Int] = []), remote
+    case local, remoteOnlySection(sections: [Int] = []), remote(force: Bool = false)
 }

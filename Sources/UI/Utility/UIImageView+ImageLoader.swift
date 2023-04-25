11e19d4481d75308ccc5f93abd0a9c999fcd6aae
miyoshi-cq <t.miyoshi@caraquri.com>
2023-04-25 23:46:44 +0900

    introduce nuke

--- Sources/UI/Utility/UIImageView+ImageLoader.swift
+++ Sources/UI/Utility/UIImageView+ImageLoader.swift
@@ -1,3 +1,4 @@
+import Nuke
 import UIKit
 
 public extension UIImageView {
@@ -6,8 +7,60 @@ public extension UIImageView {
         defaultImage: UIImage,
         adjustHeight: Bool = false
     ) {
-        Task {
-            self.image = try await ImageLoader.shared.fetch(url: url, defaultImage: defaultImage)
+        if #available(iOS 15.2, *) {
+            Task {
+                self.image = try await ImageLoader.shared.fetch(
+                    url: url,
+                    defaultImage: defaultImage
+                )
+
+                if adjustHeight {
+                    if let imageSize = self.image?.size {
+                        // 画像サイズに応じた高さに調整
+                        self.translatesAutoresizingMaskIntoConstraints = false
+                        self.heightAnchor.constraint(
+                            equalTo: self.widthAnchor,
+                            multiplier: imageSize.height / imageSize.width
+                        ).isActive = true
+                    }
+                }
+            }
+        } else {
+            self.loadImageWithNuke(
+                with: url,
+                defaultImage: defaultImage,
+                adjustHeight: adjustHeight
+            )
+        }
+    }
+
+    func loadImageWithNuke(
+        with url: String?,
+        defaultImage: UIImage?,
+        adjustHeight: Bool = false
+    ) {
+        guard let url else {
+            self.image = defaultImage
+            return
+        }
+
+        guard let url = URL(string: url) else {
+            self.contentMode = .scaleAspectFit
+            self.image = defaultImage
+            return
+        }
+
+        var request = ImageRequest(url: url)
+        request.userInfo[.imageIdKey] = url.path
+        Nuke.loadImage(with: request, into: self) { result in
+            switch result {
+            case .failure:
+                DispatchQueue.main.async {
+                    self.image = defaultImage
+                }
+            default:
+                break
+            }
 
             if adjustHeight {
                 if let imageSize = self.image?.size {

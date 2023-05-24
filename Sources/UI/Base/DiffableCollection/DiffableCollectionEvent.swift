1d4e58e86b571a75b60239d2a2c01acc3e068eab
miyoshi-cq <t.miyoshi@caraquri.com>
2023-05-24 09:48:42 +0900

    modify protocol

--- Sources/UI/Base/DiffableCollection/DiffableCollectionEvent.swift
+++ Sources/UI/Base/DiffableCollection/DiffableCollectionEvent.swift
@@ -3,20 +3,7 @@ import UIKit
 import Utility
 
 @MainActor
-public protocol DiffableCollectionEvent: FlowController {
+public protocol DiffableCollectionEvent: AnyObject {
     func didItemSelected<T>(indexPath: IndexPath, cell: T)
     func didErrorOccured(error: AppError)
-}
-
-public extension DiffableCollectionEvent where T == NavigationController {
-    @MainActor
-    func didErrorOccured(error: AppError) {
-        self.show(error: error)
-    }
-}
-
-public extension DiffableCollectionEvent where T == TabBarController {
-    func didErrorOccured(error: AppError) {
-        self.show(error: error)
-    }
 }
